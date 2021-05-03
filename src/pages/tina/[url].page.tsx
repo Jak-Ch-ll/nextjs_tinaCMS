79b92eb0f16b44b7a661887d86983a98250103f2
J.c <Jak-Ch-ll@mailbox.org>
2021-05-03 15:10:16 +0200

    Remove some unused imports

--- src/pages/tina/[url].page.tsx
+++ src/pages/tina/[url].page.tsx
@@ -1,45 +1,41 @@
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/client";
-import { BlogForm } from "../../components/tina/BlogForm";
-import { Tina } from "../../components/tina/Tina";
-import { getArticleToRender, redirectOnNoAccess } from "../../utils";
-import {
-  ArticleDB,
-  ArticleFormData,
-  ArticleRenderData,
-} from "../../utils/ArticleDB";
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/client"
+import { BlogForm } from "../../components/tina/BlogForm"
+import { Tina } from "../../components/tina/Tina"
+import { redirectOnNoAccess } from "../../utils"
+import { ArticleDB, ArticleFormData } from "../../utils/ArticleDB"
 
 interface ArticlePageProps {
-  article: ArticleFormData;
+  article: ArticleFormData
 }
 
-const db = new ArticleDB();
+const db = new ArticleDB()
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (!session) return redirectOnNoAccess();
+  const session = await getSession(context)
+  if (!session) return redirectOnNoAccess()
 
-  const url = context.params!.url as string;
+  const url = context.params!.url as string
 
   try {
-    const article = await db.getArticleFormData(url);
+    const article = await db.getArticleFormData(url)
 
     return {
       props: {
         article,
       },
-    };
+    }
   } catch {
     return {
       notFound: true,
-    };
+    }
   }
-};
+}
 
 export default function ArticleEditPage({ article }: ArticlePageProps) {
   return (
     <Tina>
       <BlogForm article={article} />
     </Tina>
-  );
+  )
 }
