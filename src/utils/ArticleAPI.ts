54552e5691c6a7669dcb131a9049e9b20acaab9f
J.c <Jak-Ch-ll@mailbox.org>
2021-05-10 12:28:52 +0200

    Remove logging of error, now received by page

--- src/utils/ArticleAPI.ts
+++ src/utils/ArticleAPI.ts
@@ -1,44 +1,32 @@
-import { Prisma } from "@prisma/client";
-import axios from "axios";
-import { API_ARTICLE_ENDPOINT_INTERNAL } from "../_constants";
+import { Prisma } from "@prisma/client"
+import axios from "axios"
+import { API_ARTICLE_ENDPOINT_INTERNAL } from "../_constants"
 
 const ax = axios.create({
   baseURL: API_ARTICLE_ENDPOINT_INTERNAL,
-});
+})
 
 export interface NewArticle
   extends Omit<Prisma.ArticleCreateInput, "id" | "createdAt" | "updatedAt"> {}
 
-export type UpdateArticle = Partial<NewArticle>;
+export type UpdateArticle = Partial<NewArticle>
 
 interface ArticleAPIInterface {
-  post: (article: NewArticle) => Promise<void>;
-  patch: (id: number, article: UpdateArticle) => Promise<void>;
-  delete: (id: number) => Promise<void>;
+  post: (article: NewArticle) => Promise<void>
+  patch: (id: number, article: UpdateArticle) => Promise<void>
+  delete: (id: number) => Promise<void>
 }
 
 export class ArticleAPI implements ArticleAPIInterface {
   public async post(data: NewArticle) {
-    try {
-      await ax.post("/", data);
-    } catch (err) {
-      console.log(err);
-    }
+    await ax.post("/", data)
   }
 
   public async patch(id: number, data: UpdateArticle) {
-    try {
-      await ax.patch(`/${id}`, data);
-    } catch (err) {
-      console.log(err);
-    }
+    await ax.patch(`/${id}`, data)
   }
 
   public async delete(id: number) {
-    try {
-      await ax.delete(`/${id}`);
-    } catch (err) {
-      console.log(err);
-    }
+    await ax.delete(`/${id}`)
   }
 }
