1289620fd3678d66a824f01ebcf5847ee23e9258
J.c <Jak-Ch-ll@mailbox.org>
2021-05-10 12:27:28 +0200

    Make home.page the blog-overview page, Remove blog.page; Add additional stylings

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,21 +1,19 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import { Banner } from "../components/Banner";
-import "../styles/globals.scss";
+import { AppProps } from "next/dist/next-server/lib/router/router"
+import { Banner } from "../components/Banner"
+import "../styles/globals.scss"
 
-import { Provider } from "next-auth/client";
-import Head from "next/head";
+import { Provider } from "next-auth/client"
+import Head from "next/head"
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Provider session={pageProps.session}>
         <Banner />
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </Provider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
