066505ff80dae5f6763aa114ec4053040aa76aea
J.c <Jak-Ch-ll@mailbox.org>
2021-06-22 17:15:01 +0200

    - Fix persist-method so it correctly awaits upload - change endpoint to use full-url to help with img-srcs

--- src/utils/ImageStore/ImageStore.ts
+++ src/utils/ImageStore/ImageStore.ts
@@ -3,62 +3,62 @@ import {
   MediaList,
   MediaListOptions,
   MediaStore,
-  MediaUploadOptions,
-} from "@tinacms/core";
-import axios from "axios";
-import slugify from "slugify";
-import { API_IMAGE_ENDPOINT_INTERNAL as endpoint } from "../../_constants";
+  MediaUploadOptions
+} from "@tinacms/core"
+import axios from "axios"
+import slugify from "slugify"
+import { API_IMAGE_ENDPOINT as endpoint } from "../../_constants"
 
 // https://tina.io/docs/media/
 
 export class ImageStore implements MediaStore {
-  accept = "image/*";
+  accept = "image/*"
 
   async persist(options: MediaUploadOptions[]): Promise<Media[]> {
-    return options.map((option) => {
-      const formData = new FormData();
-      formData.append(option.file.name, option.file);
+    return Promise.all(
+      options.map(async (option) => {
+        const formData = new FormData()
+        formData.append(option.file.name, option.file)
 
-      axios
-        .post(endpoint, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .catch((err) => {
-          throw new Error(err);
-        });
+        try {
+          await axios.post(endpoint, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
 
-      const name = slugify(option.file.name, {
-        lower: true,
-      });
+          const name = slugify(option.file.name, {
+            lower: true,
+          })
 
-      return {
-        type: "file",
-        id: option.file.name,
-        directory: `media/test`,
-        filename: option.file.name,
-        previewSrc: `${endpoint}/${option.file.name}`,
-      };
-    });
+          return {
+            type: "file" as const,
+            id: name,
+            directory: `media/test`,
+            filename: option.file.name,
+            previewSrc: `${endpoint}/${option.file.name}`,
+          }
+        } catch (err) {
+          throw new Error(err)
+        }
+      })
+    )
   }
 
-  async previewSrc(src: string) {
-    if (!src) return "";
-    return `${endpoint}/${src}`;
-  }
+  // previewSrc for rendered image in form
+  previewSrc = (src: string) => (src ? `${endpoint}/${src}` : "")
 
   async list({ limit = 10, offset = 0 }: MediaListOptions): Promise<MediaList> {
-    const res = await axios.get(endpoint);
-    const fileNames: string[] = res.data;
+    const res = await axios.get(endpoint)
+    const fileNames: string[] = res.data
 
-    const items: Media[] = [];
-    const totalCount = fileNames.length;
+    const items: Media[] = []
+    const totalCount = fileNames.length
 
     fileNames.forEach((filename) => {
-      const type = "file";
-      const id = filename;
-      const directory = `/test/`;
+      const type = "file"
+      const id = filename
+      const directory = `/test/`
 
       items.push({
         type,
@@ -66,18 +66,18 @@ export class ImageStore implements MediaStore {
         directory,
         filename,
         previewSrc: `${endpoint}/${filename}`,
-      });
-    });
+      })
+    })
 
     return {
       items,
       limit,
       offset,
       totalCount,
-    };
+    }
   }
 
   async delete(media: Media) {
-    await axios.delete(`${endpoint}/${media.filename}`);
+    await axios.delete(`${endpoint}/${media.filename}`)
   }
 }
